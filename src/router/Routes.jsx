7dfc2e010e62d210872bfa5240724af7ea32e8da
Takanashi <nao69alter@gmail.com>
2025-12-29 01:07:59 +0900

    余計なimport削除

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -4,7 +4,6 @@ import StandardLayout from '../layouts/StandardLayout.jsx';
 import BareLayout from '../layouts/BareLayout.jsx';
 import Loading from '../pages/Loading.jsx';
 import NotFound from '../pages/NotFound.jsx';
-import Strage01 from '../pages/Strage01.jsx';
 import pages from '../data/pages.json';
 
 const router = createBrowserRouter([
